fd1c2e6e0ad0e7f500ec618c0f41229e1c888787
Hearty Oh <heartyoh@hatiolab.com>
2019-11-27 21:20:50 +0800

    mwc-component

--- client/bootstrap.js
+++ client/bootstrap.js
@@ -1,6 +1,8 @@
+import { html } from 'lit-element'
 import { store } from '@things-factory/shell'
 import { TOOL_POSITION } from '@things-factory/layout-base'
 import { APPEND_APP_TOOL } from '@things-factory/apptool-base'
+import '@material/mwc-textfield'
 
 console.log(`
 ▄▄▄▄ ▄▄▄▄▄           ▄▄▄▄▄
@@ -14,7 +16,19 @@ export default function bootstrap() {
   store.dispatch({
     type: APPEND_APP_TOOL,
     tool: {
-      template: 'STowD',
+      template: html`
+        <span>STowD</span>
+      `,
+      position: TOOL_POSITION.FRONT
+    }
+  })
+
+  store.dispatch({
+    type: APPEND_APP_TOOL,
+    tool: {
+      template: html`
+        <mwc-textfield class="left right" ; label="My Textfield" iconTrailing="search" outlined> </mwc-textfield>
+      `,
       position: TOOL_POSITION.FRONT
     }
   })
