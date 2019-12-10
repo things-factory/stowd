0578be30a4b03ff1a3b74abc63cefc571d783036
hoppaang <hoppaang@hatiolab.com>
2019-12-10 17:08:16 +0900

    marker icon test

--- client/locations/marker-icons.js
+++ client/locations/marker-icons.js
@@ -1,28 +1,47 @@
 const ICON_TEMPLATE = `<?xml version="1.0" encoding="utf-8"?>
-<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 19.8 19.8" style="enable-background:new 0 0 19.8 19.8;" xml:space="preserve">
-<g fill="{{fillcolor}}">
-<rect x="4.7" y="12.3" width="10.4" height="0.5"/>
-<rect x="4.7" y="13.8" width="10.4" height="0.5"/>
-<rect x="4.7" y="15.4" width="10.4" height="0.5"/>
-<polygon points="4.7,17.4 9.7,19.4 15.1,17.4 15.1,16.9 4.7,16.9 "/>
-<path d="M9.9,0.4L0,6.5L0,8h1.1v9.5h2.2v-6.7h13.2v6.7h2.2V8h1.1l0-1.4L9.9,0.4z M5.2,9.5H3.3V8.6h1.8V9.5z M7.4,9.5H5.6V8.6h1.8 V9.5z M9.7,9.5H7.9V8.6h1.8V9.5z M12,9.5h-1.8V8.6H12V9.5z M14.2,9.5h-1.8V8.6h1.8V9.5z M16.5,9.5h-1.8V8.6h1.8V9.5z"/>
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 24 24" style="enable-background:new 0 0 24 24;" xml:space="preserve">
+<style type="text/css">
+	.st0{opacity:0.15;enable-background:new    ;}
+	.st1{fill:#FFFFFF;}
+	.st2{fill:#D56761;}
+</style>
+<g id="Layer_2">
+	<path class="st0" d="M14.4,1.3c-5.1-0.5-9.6,3-10.1,8s3.1,8.9,7.8,14c5.6-3.9,10-7,10.6-12C23.1,6.4,19.4,1.9,14.4,1.3z"/>
+	<ellipse class="st1" cx="12" cy="10.3" rx="8.5" ry="8.5"/>
+</g>
+<g id="Layer_1">
+	<path class="st2" d="M12,0.9c-5.1,0-9.2,4-9.2,9c0,5.1,4,8.5,9.2,13.1c5.2-4.5,9.2-8,9.2-13.1C21.2,4.9,17.1,0.9,12,0.9z M12,18
+		c-4.4,0-8-3.6-8-8s3.6-8,8-8s8,3.6,8,8S16.4,18,12,18z"/>
+	<g>
+		<polygon class="st2" points="6.5,7.2 6.5,13 12.5,15.9 12.5,10.4 17,8.1 17,12.9 17.8,13.2 17.8,13.2 17.8,6.9 11.7,9.9 
+			11.7,14.7 7.3,12.5 7.3,7.7 13.1,4.6 13.1,4.5 12.3,4.1 		"/>
+		<polygon class="st2" points="15.5,5.7 14.6,5.3 8.8,8.5 8.8,10.1 9.6,10.5 9.6,8.9 		"/>
+	</g>
 </g>
 </svg>
 `
 const FOCUS_ICON_TEMPLATE = `<?xml version="1.0" encoding="utf-8"?>
-<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 24 24" style="enable-background:new 0 0 24 24;" xml:space="preserve">
-<g fill="{{fillcolor}}">
-<polygon points="8.9,9.6 12.5,11.5 16.1,9.7 12.5,7.8 "/>
-<polygon points="8.4,14 12.1,15.8 12.1,12.2 8.4,10.3 "/>
-<polygon points="13,15.8 16.6,14 16.6,10.4 13,12.2 "/>
-<path d="M20.2,4.3c-0.5-0.7-1-1.4-1.6-1.9l-6-2.3l-6,2.3C5.8,3,5.3,3.7,4.8,4.5C3.9,6.1,3.3,7.9,3.3,9.8c0,5.5,4,9.3,9.2,14.2 c5.2-4.9,9.2-8.7,9.2-14.2C21.7,7.8,21.2,5.9,20.2,4.3z M17.5,14.5l-5,2.5l-5-2.5V9.4l5-2.5l5,2.5V14.5z M19.3,8l-6.7-3.3L5.8,8.1 L5.4,7.4l6.1-3.1l1-0.5l1,0.5l6.1,3L19.3,8z"/>
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 24 24" style="enable-background:new 0 0 24 24;" xml:space="preserve">
+<style type="text/css">
+	.st0{opacity:0.15;}
+	.st1{fill:#D56761;}
+	.st2{fill:#FFFFFF;}
+</style>
+<path class="st0" d="M14.4,1.3c-5.1-0.5-9.6,3-10.1,8c-0.5,5,3.1,8.9,7.8,14c5.6-3.9,10-7,10.6-12C23.1,6.4,19.4,1.9,14.4,1.3z"/>
+<path class="st1" d="M12,0.9c-5.1,0-9.2,4-9.2,9c0,5.1,4,8.5,9.2,13.1c5.2-4.5,9.2-8,9.2-13.1C21.2,5,17.1,0.9,12,0.9z"/>
+<g>
+	<polygon class="st2" points="5.9,7 5.9,13.2 12.4,16.3 12.4,10.4 17.3,8 17.3,13.1 18.1,13.5 18.1,13.5 18.1,6.6 11.6,9.9 11.6,15 
+		6.7,12.6 6.7,7.5 13,4.1 13,4.1 12.1,3.7 	"/>
+	<polygon class="st2" points="15.6,5.3 14.6,4.9 8.4,8.3 8.4,10.1 9.2,10.5 9.2,8.8 	"/>
 </g>
 </svg>
 `
 
-export const ICONS = ['black', 'red', 'blue', 'yellow', 'orange', 'tomato'].map(
-  color => 'data:image/svg+xml;charset=UTF-8;base64,' + btoa(ICON_TEMPLATE.replace('{{fillcolor}}', color))
-)
+// export const ICONS = ['black', 'red', 'blue', 'yellow', 'orange', 'tomato'].map(
+//   color => 'data:image/svg+xml;charset=UTF-8;base64,' + btoa(ICON_TEMPLATE.replace('{{fillcolor}}', color))
+// )
 
-export const FOCUS_ICON =
-  'data:image/svg+xml;charset=UTF-8;base64,' + btoa(FOCUS_ICON_TEMPLATE.replace('{{fillcolor}}', 'brown'))
+// export const FOCUS_ICON =
+//   'data:image/svg+xml;charset=UTF-8;base64,' + btoa(FOCUS_ICON_TEMPLATE.replace('{{fillcolor}}', 'brown'))
